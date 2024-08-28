80d0c3f76c52f3373b3753497abaebbdc22e77f7
crownaintanoob <70343800+crownaintanoob@users.noreply.github.com>
2024-08-28 19:43:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 @app.route('/passesinventory'­, methods=['GET']))
 def hello_world():
     print(request.args['args1'])
-    return request.args['args1']
+    return "ergerg" #request.args['args1']
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=54453)
