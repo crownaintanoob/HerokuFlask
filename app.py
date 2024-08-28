38548f801e4c7426fa676f376f692a50d290bc91
crownaintanoob <70343800+crownaintanoob@users.noreply.github.com>
2024-08-28 19:41:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/passesinventory')
+@app.route('/passesinventory'­, methods=['GET']))
 def hello_world():
     print(request.args['args1'])
     return request.args['args1']
+if __name__ == '__main__':
+    app.run(debug=True)
